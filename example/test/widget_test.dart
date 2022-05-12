1799078116c9e25ab2f71dd01b3a01a64fc1dc86
Joey Boerwinkel <joey@iconica.nl>
2022-05-12 16:28:26 +0200

    add test harness to component library

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,5 +1,26 @@
+import 'package:example/main.dart';
+import 'package:example/src/components/counter_doubler.dart';
+import 'package:flocse/flocse.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('test', () {});
+  group('Counter doubler', () {
+    late ComponentHarness harness;
+    late CounterDoublerComponent sut;
+
+    setUp(() {
+      sut = CounterDoublerComponent();
+      harness = ComponentHarness.mount(component: sut);
+    });
+
+    test('should double the counter', () async {
+      var event = IncrementEvent(2);
+
+      await harness.sendTest(event);
+
+      expect(harness.noEvents(), isTrue);
+
+      expect(event.value, equals(4));
+    });
+  });
 }
